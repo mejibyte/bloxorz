fdb51ae281a6e5f764880120abcee65383ace0af
Andrés Mejía <andmej@gmail.com>
2011-09-07 19:25:08 -0500

    Adding basic Xcode skeleton.

---
+++ Code/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  Bloxorz
+//
+//  Created by Andrés Mejía on 07/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << argv[0] << std::endl;
+    return 0;
+}
+
