f661d493da9cee8123e4bb14ba5b482b1e282472
Andrés Mejía <andmej@gmail.com>
2011-09-14 00:09:26 -0500

    Adding key handlers

--- Code/KeyboardController.cpp
+++ Code/KeyboardController.cpp
@@ -3,7 +3,7 @@
 
 void KeyboardController::prepare() {
     printf("Preparing game from KeyboardController\n");
-    board = Board("boards/1.txt");
+    board = Board("./boards/1.txt");
     view = new TwoDimensionalView(board);
     
 }
@@ -30,6 +30,30 @@ void KeyboardController::specialKeyPressed(int key){
     }
     glutPostRedisplay();
     printf("Special key pressed: %d.\n", key);
+    if (key == GLUT_KEY_UP){
+        printf("[CONTROLLER] Pressed UP key.\n");
+        board.moveTile(Board::Up);
+        
+    } else if (key == GLUT_KEY_DOWN) {
+        printf("[CONTROLLER] Pressed DOWN key.\n");        
+        board.moveTile(Board::Down);        
+        
+    } else if (key == GLUT_KEY_LEFT) {
+        printf("[CONTROLLER] Pressed LEFT key.\n");        
+        board.moveTile(Board::Left);                
+        
+    } else if (key == GLUT_KEY_RIGHT) {
+        printf("[CONTROLLER] Pressed RIGHT key.\n");        
+        board.moveTile(Board::Right);        
+        
+    }
+    if (!board.isLosingPosition()){
+        printf("[CONTROLLER] Not losing position. Should refresh view.\n");
+        view->setBoard(board);
+        refreshView();
+    } else {
+        printf("[CONTROLLER] Losing position!\n");
+    }
 }
 
 void KeyboardController::normalKeyPressed(unsigned char key){
