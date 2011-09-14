bb3a0b372cab87de99255adaff598162da479212
andevil <andresitogar@gmail.com>
2011-09-13 23:47:54 -0500

    Adding the KeyFunctions I'm working on Windows, so change the #include <GL/glut.h>

--- Code/KeyboardController.cpp
+++ Code/KeyboardController.cpp
@@ -1,4 +1,5 @@
 #include "KeyboardController.h"
+#include <GL/glut.h> 
 
 void KeyboardController::prepare() {
     printf("Preparing game from KeyboardController\n");
@@ -15,12 +16,41 @@ void KeyboardController::refreshView() {
     view->refresh();
 }
 void KeyboardController::specialKeyPressed(int key){
-    // TODO: Do something here.
+   if(key == GLUT_KEY_UP) {
+      board.moveTile(up);
+    }
+    if(key == GLUT_KEY_DOWN) {
+        board.moveTile(down);
+    }
+    if(key == GLUT_KEY_LEFT) {
+        board.moveTile(left);
+    }
+    if(key == GLUT_KEY_RIGHT) {
+        board.moveTile(right);
+    }
+    glutPostRedisplay();
     printf("Special key pressed: %d.\n", key);
 }
 
 void KeyboardController::normalKeyPressed(unsigned char key){
-    // TODO: Do something here.
+    switch (key) {
+      case 27:
+         exit(0);
+         break;
+      case 'a':		
+            board.moveTile(left);
+            break;
+	   case 's':		
+            board.moveTile(down);
+            break;
+		case 'd':		
+			board.moveTile(right);
+			break;
+		case 'w':		
+			board.moveTile(up);
+			break;
+	}
+	glutPostRedisplay();
     printf("%c was pressed.\n", key);
 }
 
