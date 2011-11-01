8ae9ced32c3eb652a3d584827175a55ed6bfd7e6
Cristhian <andresitogar@gmail.com>
2011-11-01 16:34:06 -0500

    Adding three Dimensional View.h

---
+++ Code/ThreeDimensionalView.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <stdlib.h>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#include <OpenGL/OpenGL.h>
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#elif defined _WIN32 || defined _WIN64
+#include <GL/glut.h>
+#endif
+
+#include <string>
+#include <vector>
+   
+
+#include "AbstractView.h"
+#include "Board.h"
+
+class ThreeDimensionalView : public AbstractView {
+public:
+    ThreeDimensionalView();
+    ThreeDimensionalView(Board board);    
+    void setBoard(Board board);
+    void refresh();
+    void reshape(int newWidth, int newHeight);
+    void setMessage(const char * message);
+    void clearMessage();    
+private:
+    //int cellWidth();
+    //int cellHeight();    
+    void drawCubeAt(int row, int col,        float color[]);
+    string message;
+};
